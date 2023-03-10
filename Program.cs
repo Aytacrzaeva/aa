661e851855afd19cb37b73a7747311f1091057c1
Ayynurr <aynurrmzz@gmail.com>
2023-03-10 18:38:19 +0400

    new

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] massiv = { 1, 1, 2, 3, 3, 5, 4, 6, 7, 1 };
+            int max = 0;
+            int reqem = 0;
+            int encoxtekrarlanan = 0;
+            for (int i = 0; i < massiv.Length; i++)
+            {
+                for (int j = 0; j < massiv.Length; j++) ;
+                if (massiv[i] == massiv[j])
+                {
+                    reqem++;
+
+
+                }
+
+            }
+
+
+
+
+
+
+
+
+        }
+    }
+}
+
